b817047b6834b58e1bb035f08dadce2b92e47a8e
teguh santosa <impactfort@gmail.com>
2025-06-19 01:16:27 +0700

    Update file index.html

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app.js";
+import { getFirestore } from "firebase/firestore.js";
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
