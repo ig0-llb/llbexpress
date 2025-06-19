294e3faaa6a916a0c53ccc7caa422789e9377f94
teguh santosa <impactfort@gmail.com>
2025-06-19 15:20:07 +0700

    update terbaru llbexpress

--- public/main.js
+++ public/main.js
@@ -1,16 +1,27 @@
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 import { db } from "./firebaseConfig.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { app } from "./firebaseConfig.js";
 
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("Masih login sebagai:", user.email);
+    tambahUser(); // jalankan hanya jika login aktif
+  } else {
+    window.location.href = "login.html"; // redirect kalau belum login
+  }
+});
 async function tambahUser() {
   try {
     const docRef = await addDoc(collection(db, "users"), {
       nama: "Teguh",
       umur: 25
     });
+    document.getElementById("statusUser").textContent = "✅ Data berhasil ditambahkan!";
     console.log("Dokumen berhasil ditambahkan dengan ID:", docRef.id);
   } catch (error) {
     console.error("Error menambahkan dokumen:", error);
   }
 }
-
-tambahUser();
